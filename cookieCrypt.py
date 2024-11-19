f08ba6464951014f8e62fa3a4339eca7bd53f299
epicoreo <oredgaming@gmail.com>
2024-11-19 10:14:47 -0500

    its boken :(

--- cookieCrypt.py
+++ cookieCrypt.py
@@ -1,27 +1,55 @@
 from hashlib import sha256
 import random
 from itertools import count
-# Split message into 16 byte chunks
-# Hash key into sha256 and split into 2 sections and rehash those sections
+import numpy as np
 
-SWAP_VALUES = {
-  0x02: 0x07,
-  0x04: 0x04,
-  0x09: 0x06,
-  0x07: 0x00,
-  0x00: 0x02,
-  0x0c: 0x0b,
-  0x0a: 0x09,
-  0x08: 0x0d,
-  0x0d: 0x03,
-  0x01: 0x08,
-  0x03: 0x0e,
-  0x0f: 0x0f,
-  0x06: 0x0a,
-  0x05: 0x05,
-  0x0e: 0x01,
-  0x0b: 0x0c,
-}
+
+#? Hash key into sha256 and split into 2 sections and rehash those sections
+#? perform the swap op by offsettign the swap values by the key
+
+
+SWAP_VALUES = [
+  [0x02, 0x07],
+  [0x04, 0x04],
+  [0x09, 0x06],
+  [0x07, 0x00],
+  [0x00, 0x02],
+  [0x0c, 0x0b],
+  [0x0a, 0x09],
+  [0x08, 0x0d],
+  [0x0d, 0x03],
+  [0x01, 0x08],
+  [0x03, 0x0e],
+  [0x0f, 0x0f],
+  [0x06, 0x0a],
+  [0x05, 0x05],
+  [0x0e, 0x01],
+  [0x0b, 0x0c],
+]
+
+
+def swap(char, offset = 3):
+  tmpSawp = np.array(SWAP_VALUES.copy())
+  newSwap = {}
+  # carry = tmpSawp[0][1]
+  
+  col1 = tmpSawp[:, 0:1]
+  col2 = tmpSawp[:, 1:2]
+  
+  for i, item in enumerate(col1):
+    
+    localOffset = i - offset
+    
+    if localOffset < 0:
+      localOffset = 15 - i
+    
+    print(hex(item[0]), hex(tmpSawp[localOffset, 1]))
+  
+  
+
+def unswap(char, offset):
+  pass
+
 
 def encrypt(message: bytearray, key: bytes):
   hashedKey = sha256(key.hex().encode("utf-16")).hexdigest()
@@ -36,8 +64,8 @@ def encrypt(message: bytearray, key: bytes):
   for char in message:
     print(char.to_bytes().hex())
 
-  print()
-  print(hex(0x1 + 0xf))
+  print(swap(message[2].to_bytes().hex()))
+
 
   return "Done"
 
